f5f5addaab71b93e58d86fd793787ab38c969139
mintbird <the.oppertunity@gmail.com>
2017-04-19 23:34:37 +0700

    Drop v3 support

--- acf-image-select.php
+++ acf-image-select.php
@@ -37,26 +37,8 @@ class acf_field_image_select_plugin
 		// version 4+
 		add_action('acf/register_fields', array($this, 'register_fields'));
 
-		// version 3-
-		add_action('init', array( $this, 'init' ), 5);
 	}
 
-
-	/*
-	*  Init
-	*
-	*  @description:
-	*  @since: 3.6
-	*  @created: 04/02/2014
-	*/
-
-	function init()
-	{
-		if(function_exists('register_field'))
-		{
-			register_field('acf_field_image_select', plugin_dir_path(__FILE__) . '/image-select-v3.php');
-		}
-	}
 
 	/*
 	*  register_fields
